8a271887192a69ed396d262f26b13ffcf1c4d8b5
VeryFach <162655143+VeryFach@users.noreply.github.com>
2025-11-13 14:20:23 +0700

    implement group management features with team details, chat, and project

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,16 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  config.transformer = {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    ...config.transformer,
-  };
-  config.resolver = {
-    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...config.resolver.sourceExts, "svg"],
-    ...config.resolver,
-  };
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
 
-  return config;
-})();
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
+module.exports = config;
